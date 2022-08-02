689af8b328efeef96831babc355243707509d76e
Snehjaiswal <snehj121@gmail.com>
2022-08-02 17:13:17 +0530

    update

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
